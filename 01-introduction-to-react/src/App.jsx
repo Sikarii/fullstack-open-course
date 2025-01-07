f83d01bd719ca6f79950fa9b2323acaf76544e0c
Sikari <37634135+Sikarii@users.noreply.github.com>
2025-01-07 23:32:20 +0200

    Exercise 3 for introduction-to-react

--- 01-introduction-to-react/src/App.jsx
+++ 01-introduction-to-react/src/App.jsx
@@ -5,16 +5,22 @@ import Total from "./components/Total";
 export default function App() {
   const course = "Half Stack application development";
 
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
 
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
 
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
-  const exercisesTotal = exercises1 + exercises2 + exercises3;
+  const exercisesTotal = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
@@ -22,11 +28,8 @@ export default function App() {
 
       <Content
         part1={part1}
-        exercises1={exercises1}
         part2={part2}
-        exercises2={exercises2}
         part3={part3}
-        exercises3={exercises3}
       />
 
       <Total total={exercisesTotal} />
