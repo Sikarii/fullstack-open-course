47df0a9cd5ceb7514ae2dcef058e6d9198a3c05e
Sikari <37634135+Sikarii@users.noreply.github.com>
2025-01-07 23:57:54 +0200

    Exercise 7 for introduction-to-react

--- 01-introduction-to-react/src/App.jsx
+++ 01-introduction-to-react/src/App.jsx
@@ -5,6 +5,10 @@ export default function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = (good + neutral + bad);
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
   return (
     <div>
       <div>
@@ -24,6 +28,9 @@ export default function App() {
           <p>Good: {good}</p>
           <p>Neutral: {neutral}</p>
           <p>Bad: {bad}</p>
+          <p>All: {all}</p>
+          <p>Average: {average}</p>
+          <p>Positive: {positive}%</p>
         </div>
       </div>
     </div>
