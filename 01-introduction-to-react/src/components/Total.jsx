4adab3e4645343a40b984a9bb1e3dfc69c94267a
Sikari <37634135+Sikarii@users.noreply.github.com>
2025-01-07 23:36:03 +0200

    Exercise 4 for introduction-to-react

--- 01-introduction-to-react/src/components/Total.jsx
+++ 01-introduction-to-react/src/components/Total.jsx
@@ -1,7 +1,9 @@
 export default function Total(props) {
+  const total = props.parts.reduce((sum, p) => p.exercises + sum, 0);
+
   return (
     <p>
-      Number of exercises: {props.total}
+      Number of exercises: {total}
     </p>
   );
 };
