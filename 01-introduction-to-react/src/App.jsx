561932f1cd44ff99be1581f561e4eacd5a5f6bd8
Sikari <37634135+Sikarii@users.noreply.github.com>
2025-01-07 23:47:35 +0200

    Exercise 6 for introduction-to-react

--- 01-introduction-to-react/src/App.jsx
+++ 01-introduction-to-react/src/App.jsx
@@ -1,31 +1,31 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import { useState } from "react";
 
 export default function App() {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <div>
+        <h1>Give feedback</h1>
+
+        <div>
+          <button onClick={() => setGood((v) => v + 1)}>Good</button>
+          <button onClick={() => setNeutral((v) => v + 1)}>Neutral</button>
+          <button onClick={() => setBad((v) => v + 1)}>Bad</button>
+        </div>
+      </div>
+
+      <div>
+        <h1>Statistics</h1>
+
+        <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- 01-introduction-to-react/src/components/Total.jsx
+++
@@ -1,9 +0,0 @@
-export default function Total(props) {
-  const total = props.parts.reduce((sum, p) => p.exercises + sum, 0);
-
-  return (
-    <p>
-      Number of exercises: {total}
-    </p>
-  );
-};
