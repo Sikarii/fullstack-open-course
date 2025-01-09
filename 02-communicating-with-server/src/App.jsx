19a0f475cf1c65cbeead1ed42edbde9552d3cb48
Sikari <37634135+Sikarii@users.noreply.github.com>
2025-01-10 01:40:39 +0200

    Exercise 9 for communicating-with-server

--- 02-communicating-with-server/src/App.jsx
+++ 02-communicating-with-server/src/App.jsx
@@ -6,14 +6,33 @@ export default function App() {
       name: "Arto Hellas",
       number: "040-123456",
     },
+    {
+      name: "Ada Lovelace",
+      number: "39-44-532352",
+    },
+    {
+      name: "Dan Abramov",
+      number: "12-43-234345",
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+    },
   ];
 
   const [persons, setPersons] = useState(initialPersons);
+
+  const [search, setSearch] = useState("");
 
   const [newPerson, setNewPerson] = useState({
     name: "",
     number: "",
   });
+
+  // Ideally useMemo is used here
+  const filteredPersons = persons.filter((p) =>
+    p.name.toLowerCase().indexOf(search.toLowerCase()) != -1
+  );
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -36,36 +55,49 @@ export default function App() {
     <div>
       <h2>Phonebook</h2>
 
-      <form onSubmit={addPerson}>
-        <div>
+      <div>
+        Filter shown with:
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+       />
+      </div>
+
+      <div>
+        <h1>Add new</h1>
+
+        <form onSubmit={addPerson}>
           <div>
-            Name:
-            <input
-              type="text"
-              value={newPerson.name}
-              onChange={(e) => setNewPerson((v) => ({ ...v, name: e.target.value }))}
-            />
+            <div>
+              Name:
+              <input
+                type="text"
+                value={newPerson.name}
+                onChange={(e) => setNewPerson((v) => ({ ...v, name: e.target.value }))}
+              />
+            </div>
+
+            <div>
+              Number:
+              <input
+                type="tel"
+                value={newPerson.number}
+                onChange={(e) => setNewPerson((v) => ({ ...v, number: e.target.value }))}
+              />
+            </div>
           </div>
 
           <div>
-            Number:
-            <input
-              type="tel"
-              value={newPerson.number}
-              onChange={(e) => setNewPerson((v) => ({ ...v, number: e.target.value }))}
-            />
+            <button type="submit">Add</button>
           </div>
-        </div>
-
-        <div>
-          <button type="submit">Add</button>
-        </div>
-      </form>
+        </form>
+      </div>
 
       <div>
         <h1>Numbers</h1>
         <ul>
-          {persons.map((p) =>
+          {filteredPersons.map((p) =>
             <li key={p.name}>
               <p>{p.name} {p.number}</p>
             </li>
